d3627bec196d9c48ea50f9dcde7369609a276e48
mprzymus <przymus@gmail.com>
2020-08-08 17:44:13 +0200

    show recipes moved

--- src/main/java/pl/marcin/przymus/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/pl/marcin/przymus/spring5recipeapp/controllers/IndexController.java
@@ -1,17 +1,25 @@
 package pl.marcin.przymus.spring5recipeapp.controllers;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.marcin.przymus.spring5recipeapp.repositories.CategoryRepository;
 import pl.marcin.przymus.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import pl.marcin.przymus.spring5recipeapp.services.RecipeService;
 
+@RequiredArgsConstructor
 @Controller
 @Slf4j
 public class IndexController {
-    //@RequestMapping({"", "/", "/index"})
-    String getIndexPage() {
-        log.debug("Getting index page");
-        return "index";
+    private final RecipeService recipeService;
+
+    @GetMapping({"", "/", "/index"})
+    public String showRecipes(Model model) {
+        log.debug("Getting recipes page");
+        model.addAttribute("recipes", recipeService.getRecipes());
+        return "recipes";
     }
 }
