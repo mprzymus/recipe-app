9192e4c32093b204af0ffb0a41c360a728f8858e
mprzymus <przymus@gmail.com>
2020-08-14 12:16:51 +0200

    adding recipe

--- src/main/java/pl/marcin/przymus/spring5recipeapp/controllers/RecipesController.java
+++ src/main/java/pl/marcin/przymus/spring5recipeapp/controllers/RecipesController.java
@@ -4,8 +4,11 @@ package pl.marcin.przymus.spring5recipeapp.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.marcin.przymus.spring5recipeapp.commands.RecipeCommand;
 import pl.marcin.przymus.spring5recipeapp.services.RecipeService;
 
 @Slf4j
@@ -21,5 +24,18 @@ public class RecipesController {
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
