90ad82d72cf10d4cfd9c7585e33e420b727e55b3
mprzymus <przymus@gmail.com>
2020-08-14 12:17:08 +0200

    removed printing hello

--- src/main/java/pl/marcin/przymus/spring5recipeapp/Spring5RecipeAppApplication.java
+++ src/main/java/pl/marcin/przymus/spring5recipeapp/Spring5RecipeAppApplication.java
@@ -8,7 +8,6 @@ public class Spring5RecipeAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Spring5RecipeAppApplication.class, args);
-        System.out.println("hello");
     }
 
 }
