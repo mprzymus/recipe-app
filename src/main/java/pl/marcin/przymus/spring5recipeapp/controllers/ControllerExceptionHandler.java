c3447765f6fecb112d0993d556163bdd0a6a43ab
mprzymus <przymus@gmail.com>
2020-09-06 14:12:30 +0200

    Moved specific recipe error to recipe controller

--- src/main/java/pl/marcin/przymus/spring5recipeapp/controllers/ControllerExceptionHandler.java
+++ src/main/java/pl/marcin/przymus/spring5recipeapp/controllers/ControllerExceptionHandler.java
@@ -6,22 +6,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
-import pl.marcin.przymus.spring5recipeapp.exceptions.NotFoundException;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
-
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound(Exception exception) {
-        log.error("Handling not found exception: " + exception.getMessage());
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("404error");
-        modelAndView.addObject("exception", exception);
-        return modelAndView;
-    }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(NumberFormatException.class)
